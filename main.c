b5da91b80af90d4a5025633f15ca97c13732a0af
SASANO Takayoshi <uaa@uaa.org.uk>
2025-08-10 15:47:03 +0900

    add some tests

--- main.c
+++ main.c
@@ -24,7 +24,7 @@ static int dit_on = 0, dit_off = 0, dah_on = 0, dah_off = 0;
 static int dit_total = 0, dah_total = 0;
 
 #define DITDAH_LEN 0x8000
-#define DITDAH_POS 0x0800
+#define DITDAH_POS 0x2000
 
 #define CALIB_LEN 0x2000
 #define CALIB_POS 0x0800
@@ -94,10 +94,93 @@ static char detect_element(int v)
 	else return '-';
 }
 
-static char *check_ditdah_memory(int fd, unsigned char sig0, int sig0_on_delay, int sig0_off_delay, unsigned char sig1, int sig1_on_delay, int sig1_off_delay, int offset, int width)
+static char *check_squeeze(int fd, unsigned char sig0, int sig0_on_delay, int sig0_off_delay, unsigned char sig1, int sig1_on_delay, int sig1_off_delay, int offset, int width, char *result_str, int results)
 {
-	int n, t0, t1, m, u[RESULTS];
-	static char result_str[RESULTS / 2 + 1];
+	int n, t0, t1, m, *u;
+	struct event *ev, *ev0;
+
+	set_maxpos(fd, DITDAH_LEN);
+
+	ev = add_event_entry(ev0 = eventbuf, 0, 0, EVT_SET);
+	ev = add_event_entry(ev, DITDAH_POS, sig0, EVT_SET);
+	ev = add_event_entry(ev, 0, OUT_BIT, EVT_CHGSTS);
+	ev = add_event_entry(ev, 1, sig0 | sig1, EVT_SET);
+
+	t0 = offset + width - sig0_off_delay;
+	t1 = offset + width - sig1_off_delay;
+	if (!sig1 || t0 == t1) {
+		// no sig1, or sig0 and sig1 same timing
+		m = 0;
+	} else if (t0 < t1) {
+		// sig0 first
+		m = sig1;
+	} else {
+		// sig1 first
+		m = sig0;
+		n = t1;
+		t1 = t0;
+		t0 = n;
+	}
+	ev = add_event_entry(ev, t0, m, EVT_SET);
+	if (m)
+		ev = add_event_entry(ev, t1, 0, EVT_SET);
+
+	send_event_and_get_log(fd, ev - ev0);
+
+	ev = &unpacked_log[0];
+	n = DITDAH_LEN;
+	u = alloca(sizeof(int) * results);
+	parse_event(ev, n, u, results);
+
+	for (n = 0; n < results / 2; n++)
+		result_str[n] = detect_element(u[n * 2]);
+	result_str[n] = '\0';
+
+	return result_str;
+}
+
+static void do_squeeze(int fd)
+{
+#define SQ_RESULTS 10
+
+	int i, width, offset, step, total;
+	char result_str1[SQ_RESULTS / 2 + 1], result_str2[SQ_RESULTS / 2 + 1];
+
+	offset = dit_total / 16;
+	width = dit_total / 8;
+	step = dit_total / 4;
+	total = dit_total + dah_total;
+
+	printf("* squeeze\n");
+
+	memset(result_str1, 0, sizeof(result_str1));
+	for (i = offset; i < total * 2; i += step) {
+		check_squeeze(fd,
+			      DIT_BIT, calib_on_1, calib_off_1,
+			      DAH_BIT, calib_on_2, calib_off_2,
+			      i, width, result_str2, SQ_RESULTS);
+		if (!strcmp(result_str1, result_str2)) continue;
+		strcpy(result_str1, result_str2);
+		printf("dit + dah %2d/%2d\t%s\n",
+		       (i / step) + 1, total / step, result_str1);
+	}
+
+	memset(result_str1, 0, sizeof(result_str1));
+	for (i = offset; i < total * 2; i += step) {
+		check_squeeze(fd,
+			      DAH_BIT, calib_on_2, calib_off_2,
+			      DIT_BIT, calib_on_1, calib_off_1,
+			      i, width, result_str2, SQ_RESULTS);
+		if (!strcmp(result_str1, result_str2)) continue;
+		strcpy(result_str1, result_str2);
+		printf("dah + dit %2d/%2d\t%s\n",
+		       (i / step) + 1, total / step, result_str1);
+	}
+}
+
+static char *check_ditdah_memory(int fd, unsigned char sig0, int sig0_on_delay, int sig0_off_delay, unsigned char sig1, int sig1_on_delay, int sig1_off_delay, int offset, int width, char *result_str, int results)
+{
+	int n, t0, t1, m, *u;
 	struct event *ev, *ev0;
 
 	set_maxpos(fd, DITDAH_LEN);
@@ -134,18 +217,57 @@ static char *check_ditdah_memory(int fd, unsigned char sig0, int sig0_on_delay, 
 
 	ev = &unpacked_log[0];
 	n = DITDAH_LEN;
-	parse_event(ev, n, u, RESULTS);
+	u = alloca(sizeof(int) * results);
+	parse_event(ev, n, u, results);
 
-	for (n = 0; n < RESULTS / 2; n++)
+	for (n = 0; n < results / 2; n++)
 		result_str[n] = detect_element(u[n * 2]);
 	result_str[n] = '\0';
 
 	return result_str;
 }
 
+static void do_simple(int fd)
+{
+	int i, width, offset, step, extra;
+	char result_str1[RESULTS / 2 + 1], result_str2[RESULTS / 2 + 1];
+
+	offset = dit_total / 16;
+	width = dit_total / 8;
+	step = dit_total / 4;
+	extra = dit_total / 2;
+
+	printf("* simple\n");
+
+	memset(result_str1, 0, sizeof(result_str1));
+	for (i = offset; i < dit_total + extra; i += step) {
+		check_ditdah_memory(fd,
+				    DIT_BIT, calib_on_1, calib_off_1,
+				    0, calib_on_2, calib_off_2,
+				    i, width, result_str2, RESULTS);
+		if (!strcmp(result_str1, result_str2)) continue;
+		strcpy(result_str1, result_str2);
+		printf("dit 1-%2d/%2d\t%s\n",
+		       (i / step) + 1, dit_total / step, result_str1);
+	}
+
+	memset(result_str1, 0, sizeof(result_str1));
+	for (i = offset; i < dah_total + extra; i += step) {
+		check_ditdah_memory(fd,
+				    DAH_BIT, calib_on_2, calib_off_2,
+				    0, calib_on_1, calib_off_1,
+				    i, width, result_str2, RESULTS);
+		if (!strcmp(result_str1, result_str2)) continue;
+		strcpy(result_str1, result_str2);
+		printf("dah 1-%2d/%2d\t%s\n",
+		       (i / step) + 1, dah_total / step, result_str1);
+	}
+}
+
 static void do_ditdah_memory(int fd)
 {
 	int i, width, offset, step, extra;
+	char result_str1[RESULTS / 2 + 1], result_str2[RESULTS / 2 + 1];
 
 	offset = dit_total / 16;
 	width = dit_total / 8;
@@ -154,36 +276,28 @@ static void do_ditdah_memory(int fd)
 
 	printf("* dit/dah memory\n");
 
+	memset(result_str1, 0, sizeof(result_str1));
 	for (i = offset; i < dit_total + extra; i += step) {
+		check_ditdah_memory(fd,
+				    DIT_BIT, calib_on_1, calib_off_1,
+				    DAH_BIT, calib_on_2, calib_off_2,
+				    i, width, result_str2, RESULTS);
+		if (!strcmp(result_str1, result_str2)) continue;
+		strcpy(result_str1, result_str2);
 		printf("dit on, dah %2d/%2d\t%s\n",
-		       (i / step) + 1, dit_total / step,
-		       check_ditdah_memory(fd,
-					   DIT_BIT, calib_on_1, calib_off_1,
-					   DAH_BIT, calib_on_2, calib_off_2,
-					   i, width));
-	}
-	for (i = offset; i < dah_total + extra; i += step) {
-		printf("dah on, dit %2d/%2d\t%s\n",
-		       (i / step) + 1, dah_total / step,
-		       check_ditdah_memory(fd,
-					   DAH_BIT, calib_on_2, calib_off_2,
-					   DIT_BIT, calib_on_1, calib_off_1,
-					   i, width));
+		       (i / step) + 1, dit_total / step, result_str1);
 	}
 
-	for (i = offset; i < dit_total + extra; i += step) {
-		printf("dit 1-%2d/%2d\t%s\n", (i / step) + 1, dit_total / step,
-		       check_ditdah_memory(fd,
-					   DIT_BIT, calib_on_1, calib_off_1,
-					   0, calib_on_2, calib_off_2,
-					   i, width));
-	}
+	memset(result_str1, 0, sizeof(result_str1));
 	for (i = offset; i < dah_total + extra; i += step) {
-		printf("dah 1-%2d/%2d\t%s\n", (i / step) + 1, dah_total / step,
-		       check_ditdah_memory(fd,
-					   DAH_BIT, calib_on_2, calib_off_2,
-					   0, calib_on_1, calib_off_1,
-					   i, width));
+		check_ditdah_memory(fd,
+				    DAH_BIT, calib_on_2, calib_off_2,
+				    DIT_BIT, calib_on_1, calib_off_1,
+				    i, width, result_str2, RESULTS);
+		if (!strcmp(result_str1, result_str2)) continue;
+		strcpy(result_str1, result_str2);
+		printf("dah on, dit %2d/%2d\t%s\n",
+		       (i / step) + 1, dah_total / step, result_str1);
 	}
 }
 
@@ -334,8 +448,11 @@ static int do_main(int fd)
 menu:
 	printf("\n");
 	printf("menu:\n");
+	printf("a) do all tests\n");
 	printf("0) check dit/dah length\n");
-	printf("1) check dit/dah memory\n");
+	printf("1) check simple timing\n");
+	printf("2) check dit/dah memory\n");
+	printf("3) check squeeze\n");
 	printf("c) calibration\n");
 	printf("x) exit\n");
 
@@ -353,12 +470,20 @@ menu:
 		save_config();
 		printf(CONFIG_FILE " saved\n");
 		break;
+	case 'a':
+	case 'A':
 	case '0':
 		do_ditdah_length(fd);
-		break;
+		if (*buf == '0') break;
 	case '1':
+		do_simple(fd);
+		if (*buf == '1') break;
+	case '2':
 		do_ditdah_memory(fd);
-		break;
+		if (*buf == '2') break;
+	case '3':
+		do_squeeze(fd);
+		if (*buf == '3') break;
 	default:
 		break;
 	}
